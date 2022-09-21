c4db9b5539b0a885b22e43045ee55091371ddbaa
unknown <hazvinurafgan@gmail.com>
2022-09-21 21:34:40 +0700

    add footer

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,15 +2,19 @@
     <main>
         <Navbar />
         <p>This Home</p>
+        <Footer />
     </main>
 </template>
 
 <script>
     import Navbar from '@/components/molecules/Navbar.vue';
+    import Footer from '@/components/molecules/Footer.vue';
+
     export default{
         name:'HomePage',
         components : {
-            Navbar
+            Navbar,
+            Footer
         }
     }
 </script>
