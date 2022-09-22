cae98ab557ca50862f44845805dedba305a5bdc5
unknown <hazvinurafgan@gmail.com>
2022-09-23 00:19:53 +0700

    fix login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,6 @@
 <template>
     <main class="flex justify-center content-center">
+        
         <div class="m-5 mt-10 p-5 border-2 lg:w-[50%] pt-10 pb-10">
             <div class="flex justify-center pb-5">
                 <img :src="image" />
@@ -10,6 +11,11 @@
             <TextArea type="text" name="email" class="w-full mb-3 px-3 py-2" @onsubmits="inputChange" />
             <label class="text-sm"><span class="text-red-700">*</span>Password</label>
             <TextArea type="password" name="password" class="w-full px-3 py-2" @onsubmits="inputChange" />
+            
+            <div v-if="auth.isError">
+                <p class="pt-3 text-sm text-red-700">{{auth.errMsg}}</p>
+            </div>
+            
             <div class="mt-3 flex justify-between">
                 <div class="flex">
                     <TextArea type="checkbox" class="mr-2" />
@@ -17,6 +23,7 @@
                 </div>
                 <p class="text-xs font-bold text-blue-800 cursor-pointer">Lupa Password</p>
             </div>
+    
             <Button label="Masuk" styles="bg-secondary w-full text-white active:bg-primary h-12 font-bold mt-3" @onSubmit="loginHandler" />
             <p class="text-sm text-center pt-4">atau masuk menggunakan</p>
             <div class="border-2 p-2 rounded-md flex justify-center mt-4 active:bg-slate-400 cursor-pointer">
@@ -24,24 +31,39 @@
                 <p class="pl-3">Google</p>
             </div>
         </div>
+
     </main>
 </template>
 
 <script>
     import TextArea from '@/components/atom/TextArea.vue';
     import Button from '@/components/atom/Button.vue';
+    import { mapActions, mapGetters } from 'vuex';
+
     export default {
         name:"LoginPage",
         data(){
             return {
                 image: require('@/assets/svg/fazztrack-logo.svg'),
                 email: '',
-                password:''
+                password:'',
             }
         },
+        computed:{
+            ...mapGetters({
+                auth:"auth/LoginUsers"
+            })
+        },
         methods: {
+            ...mapActions({
+                authAction: "auth/LoginUsers"
+            }),
             loginHandler(){
-                console.log('test');
+                const body = {
+                    email : this.email,
+                    password : this.password
+                }
+                this.authAction(body)
             },
             inputChange(data){
                 if(data.name === 'email') this.email = data.value
